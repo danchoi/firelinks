1a1cf1f3f8b5e83fc99f6b81c49185c00a7784e0
Daniel Choi <dhchoi@gmail.com>
2011-07-26 11:26:42 -0400

    make sure no other elinks session is around

--- lib/firelinks.rb
+++ lib/firelinks.rb
@@ -3,22 +3,22 @@ puts "Starting firelinks"
 puts "Press CTRL-C to stop"
 sleep 1
 
-
-
 if `which elinks` !~ /\w/
   abort "Missing elinks! Please install it."
 end
 
+`killall elinks`
+
 paths = ["#{ENV['HOME']}/.mozilla/firefox", "#{ENV['HOME']}/Library/Application Support"]
 history_path = `find #{paths.map {|p| "'#{p}'"}.join(' ')} -name places.sqlite`.chomp
   
-puts "Using Firefox database: #{history_path}"
+puts "Using Firefox database: #{history_path.inspect}"
+
 if history_path == ''
   abort "You're missing your Firefox browser history database. Are you sure you have a recent version of Firefox installed?"
 end
 
 command = %Q{sqlite3 -line '#{history_path}' 'select url, title, last_visit_date from moz_places order by last_visit_date desc limit 2'}
-puts command
 
 use_remote = nil
 
